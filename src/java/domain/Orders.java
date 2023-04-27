3d00cc386a5ebbcce475c487eac8c2c9be2861cf
florrync1808 <112406006+florrync1808@users.noreply.github.com>
2023-04-27 15:56:49 +0800

    Update Orders.java

--- src/java/domain/Orders.java
+++ src/java/domain/Orders.java
@@ -3,15 +3,7 @@ package domain;
 import java.io.Serializable;
 
 public class Orders implements Serializable {
-    private String staffId;
-    private String fullName;
-    private String birthDate;
-    private String contactNo;
-    private String email;
-    private String password;
-    private String employmentStatus;
-    private String createdAt;
-    
+
     public Orders() {
     }
 
