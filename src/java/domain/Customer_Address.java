38fef40e46114260428d39d3f849e1d45da6be29
florrync1808 <112406006+florrync1808@users.noreply.github.com>
2023-04-27 15:55:52 +0800

    Update Customer_Address.java

--- src/java/domain/Customer_Address.java
+++ src/java/domain/Customer_Address.java
@@ -3,13 +3,9 @@ package domain;
 import java.io.Serializable;
 
 public class Customer_Address implements Serializable {
-    private String staffId;
-    private String fullName;
-    private String birthDate;
-    private String contactNo;
-    private String email;
-    private String password;
-    private String employmentStatus;
+    private String addressId;
+    private String customerId;
+    private boolean isPrimary;
     private String createdAt;
     
     public Customer_Address() {
