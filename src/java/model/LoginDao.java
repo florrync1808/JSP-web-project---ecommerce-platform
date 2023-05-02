acb515a223261af5f96a00b4aa3a26d0356257fe
florrync1808 <112406006+florrync1808@users.noreply.github.com>
2023-05-03 00:20:09 +0800

    staff login

--- src/java/model/LoginDao.java
+++ src/java/model/LoginDao.java
@@ -1,7 +1,6 @@
 package model;
 
 import java.sql.*;
-import java.util.ArrayList;
 
 public class LoginDao {
 
@@ -9,6 +8,53 @@ public class LoginDao {
         String userName = loginBean.getUserName(); //Assign user entered values to temporary variables.
         String password = loginBean.getPassword();
 
+        String staffLog = "";
+        String customerLog = "";
+
+        staffLog = staffLogin(userName, password);
+        customerLog = customerLogin(userName, password);
+
+        if (staffLog.equals("SUCCESS")) {
+            return "STAFFSUCCESS";
+        }
+        else if (customerLog.equals("SUCCESS")) {
+            return "CUSTOMERSUCCESS";
+        }
+        return "Invalid Login Credentials"; // Return appropriate message in case of failure
+    }
+
+    public String staffLogin(String userName, String password) {
+        Connection con = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+
+        String userNameDB = "";
+        String passwordDB = "";
+        String query = "SELECT * from STAFFS where staff_id=? and password=?";
+
+        try {
+            con = DBConnection.createConnection(); //Fetch database connection object
+            statement = con.prepareStatement(query); //Statement is used to write queries. 
+            statement.setString(1, userName);
+            statement.setString(2, password);
+            resultSet = statement.executeQuery(); //the table name is users and userName,password are columns. Fetching all the records and storing in a resultSet.
+
+            while (resultSet.next()) // Until next row is present otherwise it return false
+            {
+                userNameDB = resultSet.getString("staff_id"); //fetch the values present in database
+                passwordDB = resultSet.getString("password");
+
+                if (userName.equals(userNameDB) && password.equals(passwordDB)) {
+                    return "SUCCESS"; ////If the user entered values are already present in the database, which means user has already registered so return a SUCCESS message.
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return "Invalid";
+    }
+
+    public String customerLogin(String userName, String password) {
         Connection con = null;
         PreparedStatement statement = null;
         ResultSet resultSet = null;
@@ -23,7 +69,7 @@ public class LoginDao {
             statement.setString(1, userName);
             statement.setString(2, password);
             resultSet = statement.executeQuery(); //the table name is users and userName,password are columns. Fetching all the records and storing in a resultSet.
-            
+
             while (resultSet.next()) // Until next row is present otherwise it return false
             {
                 userNameDB = resultSet.getString("email"); //fetch the values present in database
@@ -36,6 +82,6 @@ public class LoginDao {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return "Invalid Login Credentials"; // Return appropriate message in case of failure
+        return "Invalid";
     }
 }
