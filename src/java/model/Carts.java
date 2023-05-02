4a6a8fa41514a93e970508d73b2cad8620e669a1
florrync1808 <112406006+florrync1808@users.noreply.github.com>
2023-05-02 15:53:58 +0800

    created entity class from db

---
+++ src/java/model/Carts.java
@@ -0,0 +1,103 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author cbiev
+ */
+@Entity
+@Table(name = "CARTS")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Carts.findAll", query = "SELECT c FROM Carts c"),
+    @NamedQuery(name = "Carts.findByCartId", query = "SELECT c FROM Carts c WHERE c.cartId = :cartId")})
+public class Carts implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 8)
+    @Column(name = "CART_ID")
+    private String cartId;
+    @JoinColumn(name = "CART_LIST_ID", referencedColumnName = "CART_LIST_ID")
+    @ManyToOne(optional = false)
+    private CartLists cartListId;
+    @JoinColumn(name = "CUSTOMER_ID", referencedColumnName = "CUSTOMER_ID")
+    @ManyToOne(optional = false)
+    private Customers customerId;
+
+    public Carts() {
+    }
+
+    public Carts(String cartId) {
+        this.cartId = cartId;
+    }
+
+    public String getCartId() {
+        return cartId;
+    }
+
+    public void setCartId(String cartId) {
+        this.cartId = cartId;
+    }
+
+    public CartLists getCartListId() {
+        return cartListId;
+    }
+
+    public void setCartListId(CartLists cartListId) {
+        this.cartListId = cartListId;
+    }
+
+    public Customers getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Customers customerId) {
+        this.customerId = customerId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (cartId != null ? cartId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Carts)) {
+            return false;
+        }
+        Carts other = (Carts) object;
+        if ((this.cartId == null && other.cartId != null) || (this.cartId != null && !this.cartId.equals(other.cartId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "model.Carts[ cartId=" + cartId + " ]";
+    }
+    
+}
