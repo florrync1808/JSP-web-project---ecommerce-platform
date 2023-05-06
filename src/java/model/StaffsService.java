b6a1b4a8311be0c22aaa7a382d3bfc0769e84aee
Lim Jia Qing <jiaqing8999@gmail.com>
2023-05-07 03:54:59 +0800

    Update StaffsService.java

--- src/java/model/StaffsService.java
+++ src/java/model/StaffsService.java
@@ -15,35 +15,57 @@ public class StaffsService {
         this.mgr = mgr;
     }
 
-    public boolean addItem(Staffs item) {
-        mgr.persist(item);
+    public boolean addStaff(Staffs staff) {
+        mgr.persist(staff);
         return true;
     }
 
-    public Staffs findItemByCode(String code) {
-        Staffs item = mgr.find(Staffs.class, code);
-        return item;
+    public Staffs findStaffByStaffIndex(String sIndex) {
+        Staffs staff = mgr.find(Staffs.class, sIndex);
+        return staff;
     }
 
-    public boolean deleteItem(String code) {
-        Staffs item = findItemByCode(code);
-        if (item != null) {
-            mgr.remove(item);
+    public Staffs findStaffByStaffId(String staffId) {
+        try {
+            //query that early difined in Products class
+            Query q = mgr.createNamedQuery("Staffs.findByStaffId");
+
+            //set the name into the query
+            q.setParameter("staffId", staffId);
+
+            //retrieve result
+            Staffs staff = (Staffs) q.getSingleResult();
+            return staff;
+        } catch (NoResultException ex) {
+            // if no result, then return null
+            Staffs staff = null;
+            return staff;
+        }
+    }
+
+    public boolean deleteItem(String staffId) {
+        Staffs staff = findStaffByStaffId(staffId);
+        if (staff != null) {
+            mgr.remove(staff);
             return true;
         }
         return false;
     }
 
     public List<Staffs> findAll() {
-        List itemList = mgr.createNamedQuery("Staffs.findAll").getResultList();
-        return itemList;
+        List staffsList = mgr.createNamedQuery("Staffs.findAll").getResultList();
+        return staffsList;
     }
 
-    public boolean updateItem(Staffs item) {
-        Staffs tempItem = findItemByCode(item.getStaffId());
+    public boolean updateItem(Staffs staff) {
+        Staffs tempItem = findStaffByStaffId(staff.getStaffId());
         if (tempItem != null) {
-            tempItem.setName(item.getName());
-            tempItem.setBirthdate(item.getBirthdate());
+            tempItem.setName(staff.getName());
+            tempItem.setBirthdate(staff.getBirthdate());
+            tempItem.setContactNo(staff.getContactNo());
+            tempItem.setEmploymentStatus(staff.getEmploymentStatus());
+            tempItem.setPassword(staff.getPassword());
+            tempItem.setCreatedAt(staff.getCreatedAt());
             return true;
         }
         return false;
