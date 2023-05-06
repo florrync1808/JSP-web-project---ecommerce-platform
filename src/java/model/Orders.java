c9e368b6d6c088f0e6262d3e33fe4ca015c77ad1
VallerieSee <valleriesee@gmail.com>
2023-05-06 23:58:22 +0800

    123

--- src/java/model/Orders.java
+++ src/java/model/Orders.java
@@ -1,4 +1,7 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package model;
 
 import java.io.Serializable;
@@ -22,12 +25,17 @@ import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
+/**
+ *
+ * @author End User
+ */
 @Entity
 @Table(name = "ORDERS")
 @XmlRootElement
 @NamedQueries({
     @NamedQuery(name = "Orders.findAll", query = "SELECT o FROM Orders o"),
     @NamedQuery(name = "Orders.findByOrderId", query = "SELECT o FROM Orders o WHERE o.orderId = :orderId"),
+    @NamedQuery(name = "Orders.findByStaffId", query = "SELECT o FROM Orders o WHERE o.staffId = :staffId"),
     @NamedQuery(name = "Orders.findByShippingAddress", query = "SELECT o FROM Orders o WHERE o.shippingAddress = :shippingAddress"),
     @NamedQuery(name = "Orders.findByRecipientName", query = "SELECT o FROM Orders o WHERE o.recipientName = :recipientName"),
     @NamedQuery(name = "Orders.findByRecipientContactNo", query = "SELECT o FROM Orders o WHERE o.recipientContactNo = :recipientContactNo"),
@@ -41,6 +49,9 @@ public class Orders implements Serializable {
     @Size(min = 1, max = 8)
     @Column(name = "ORDER_ID")
     private String orderId;
+    @Size(max = 8)
+    @Column(name = "STAFF_ID")
+    private String staffId;
     @Basic(optional = false)
     @NotNull
     @Size(min = 1, max = 255)
@@ -69,9 +80,9 @@ public class Orders implements Serializable {
     @JoinColumn(name = "PAYMENT_ID", referencedColumnName = "PAYMENT_ID")
     @ManyToOne
     private Payments paymentId;
-    @JoinColumn(name = "STAFF_ID", referencedColumnName = "STAFF_ID")
+    @JoinColumn(name = "STAFF_INDEX", referencedColumnName = "STAFF_INDEX")
     @ManyToOne
-    private Staffs staffId;
+    private Staffs staffIndex;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "orderId")
     private List<OrderStatuses> orderStatusesList;
 
@@ -96,6 +107,14 @@ public class Orders implements Serializable {
 
     public void setOrderId(String orderId) {
         this.orderId = orderId;
+    }
+
+    public String getStaffId() {
+        return staffId;
+    }
+
+    public void setStaffId(String staffId) {
+        this.staffId = staffId;
     }
 
     public String getShippingAddress() {
@@ -155,12 +174,12 @@ public class Orders implements Serializable {
         this.paymentId = paymentId;
     }
 
-    public Staffs getStaffId() {
-        return staffId;
+    public Staffs getStaffIndex() {
+        return staffIndex;
     }
 
-    public void setStaffId(Staffs staffId) {
-        this.staffId = staffId;
+    public void setStaffIndex(Staffs staffIndex) {
+        this.staffIndex = staffIndex;
     }
 
     @XmlTransient
